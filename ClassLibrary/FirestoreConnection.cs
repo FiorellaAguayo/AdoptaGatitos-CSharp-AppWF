b16b847440b713d538306d4060c1401a82d6534f
Fiorella Aguayo <fiorelamarina12@gmail.com>
2023-07-12 02:10:53 -0300

    actualizo abm de usuarios y mascotas

--- ClassLibrary/FirestoreConnection.cs
+++ ClassLibrary/FirestoreConnection.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Google.Cloud.Firestore;
 
-namespace ClassLibrary
+namespace Firestore
 {
     public class FirestoreConnection
     {
